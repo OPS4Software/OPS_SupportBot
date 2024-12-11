40f9793a4c2ada3c4df0d1113ffec7673d9b4dc2
sava2109 <77697503+sava2109@users.noreply.github.com>
2024-12-11 02:53:45 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = '8048264863:AAFVxtPzlEJBm_3VFsRNmRUOKMtUA23Kc60'
+CLICKUP_TOKEN = 'pk_152605665_FP88B5USRFZSLIRTWV1A74JXHYQ9X1RY'
+SUPPORTCHAT_PAY2M = '-4544953571'
+
+# Web app configuration
+WEBAPP_URL = "https://prmanagment.org/webapp"  # You'll need to replace this with your actual deployed webapp URL
+
+# ClickUp configuration
+CLICKUP_LIST_ID = "901804306790"
+CLICKUP_TEAM_ID = "9018636739"
